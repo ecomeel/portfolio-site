55c5fe4416b2dbaa7d2cba918e80c60ea2987acf
ecomeel <ecomeel@gmail.com>
2024-02-13 15:24:58 +0300

    burger menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,30 @@
-import {FC} from 'react'
+import { FC } from "react";
 
 import { Logo } from "./logo/Logo";
-import { Menu } from './menu/Menu';
-import styles from './header.module.scss'
+import { Menu } from "./menu/Menu";
+import styles from "./header.module.scss";
 
-export const Header: FC= () => {
+export const Header: FC = () => {
   return (
     <header className={styles.header}>
-      <div className="container">
+      <div className={`container ${styles.container}`}>
         <Logo />
         <Menu />
+        <ul id="menuList" className={styles.menu}>
+          <li className={styles.item}>
+            <a href="#portfolio">портфолио</a>
+          </li>
+          <li className={styles.item}>
+            <a href="#competencies">компетенции</a>
+          </li>
+          <li className={styles.item}>
+            <a href="#services">услуги</a>
+          </li>
+          <li className={styles.item}>
+            <a href="#footer">контакты</a>
+          </li>
+        </ul>
       </div>
     </header>
-  )
-}
- 
+  );
+};
--- src/components/header/menu/Menu.tsx
+++ src/components/header/menu/Menu.tsx
@@ -5,12 +5,29 @@ import styles from "./menu.module.scss";
 
 export const Menu: FC = () => {
   function handleBurgerClick() {
-
+    const menuListNode = document.getElementById("menuList");
+    const burgerBtnNode = document.getElementById('burgerBtn')
+    menuListNode?.classList.toggle("active-burger");
+    burgerBtnNode?.classList.toggle("rotate-icon");
   }
   return (
     <nav className={styles.menu}>
-      <Navigation />
-      <button className={styles.burger} onClick={handleBurgerClick}></button>
+      {/* <Navigation /> */}
+      <ul className={styles.menu}>
+        <li className={styles.item}>
+          <a href="#portfolio">портфолио</a>
+        </li>
+        <li className={styles.item}>
+          <a href="#competencies">компетенции</a>
+        </li>
+        <li className={styles.item}>
+          <a href="#services">услуги</a>
+        </li>
+        <li className={styles.item}>
+          <a href="#footer">контакты</a>
+        </li>
+      </ul>
+      <button id="burgerBtn" className={styles.burger} onClick={handleBurgerClick}></button>
     </nav>
   );
 };
