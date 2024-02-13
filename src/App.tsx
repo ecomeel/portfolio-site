9a1e561b0e1747280dd17c4957745988900511e8
ecomeel <ecomeel@gmail.com>
2024-02-13 16:58:50 +0300

    hide overflow x

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-// import { register } from "swiper/element/bundle";
-
 import "./App.scss";
 import { Header } from "./components/header/Header";
 import { Content } from "./components/content/Content";
 import { Footer } from "components/footer/Footer";
 
 function App() {
-  // register()
   return (
     <>
       <Header />
